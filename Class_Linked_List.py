8c4a76daf447898cd95d9432a21cc2908a6c99b9
Sebastian Lopez <sebastian.lopez@berkeley.edu>
2022-05-05 11:09:41 -0700

    First commit

---
+++ Class_Linked_List.py
@@ -0,0 +1,75 @@
+from tempfile import tempdir
+
+#This class acts as a dictionary. Stores the values and pointer of the Linked List
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node 
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True 
+             
+    def pop(self, value):
+        if self.head ==0:
+            return None
+        temp = self.head 
+        pre = self.head 
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -=1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp 
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            # New node is pointing where Head was pointing to.
+            new_node.next = self.head
+            #Head is pointing to the new_node
+            self.head = new_node
+        self.length +=1
+        return True 
+        
+
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.prepend(0)
+
+my_linked_list.print_list()
+
+
