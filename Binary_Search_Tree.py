62979120a94e7deb90657e04623b2733ba225ecf
Sebastian Lopez <sebastian.lopez@berkeley.edu>
2022-05-05 19:06:21 -0700

    implementing DBS post_order

--- Binary_Search_Tree.py
+++ Binary_Search_Tree.py
@@ -69,16 +69,28 @@ class BinarySearchTree:
 
     def deepth_BS_pre_order(self):
         results = []
-        
         def traverse(current_node):
             results.append(current_node.value)
             if current_node.left is not None:
                 traverse(current_node.left)
             if current_node.right is not None:
                 traverse(current_node.right)
-
         traverse(self.root)
         return results
+
+    def deepth_BS_post_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+        traverse(self.root)
+        return results
+
+
+    
 
 
 
