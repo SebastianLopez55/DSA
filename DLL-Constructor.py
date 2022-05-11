7d1e3ef9c3e2d63f1e651770e2aaf96ba4e23b6d
SebastianLopez55 <lopezsebastian856@gmail.com>
2022-05-10 22:32:01 -0700

    add the append method to the double linked list class

--- DLL-Constructor.py
+++ DLL-Constructor.py
@@ -1,8 +1,8 @@
 class Node:
     def __init__(self, value):
         self.value = value
-        self.next = None
-        self.prev = None
+        self.next = None #arrow pointing right
+        self.prev = None #arrow pointing left
         
 
 class DoublyLinkedList:
@@ -12,6 +12,18 @@ class DoublyLinkedList:
         self.tail = new_node
         self.length = 1
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node # Pointing the tail of the last node added to the list to the "new_node"
+            new_node.prev = self.tail # The pointer to the previous node "prev" will point where "tail" is pointing 
+            self.tail = new_node
+        self.length +=1
+        return True 
+
     def print_list(self):
         temp = self.head
         while temp is not None:
@@ -20,6 +32,7 @@ class DoublyLinkedList:
   
 
 my_doubly_linked_list = DoublyLinkedList(7)
+my_doubly_linked_list.append(2)
 
 my_doubly_linked_list.print_list()
 
