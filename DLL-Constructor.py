6d450fa1f72d8e9bf7b632e5abf972f28f91f1e4
SebastianLopez55 <lopezsebastian856@gmail.com>
2022-05-10 22:49:33 -0700

    add the pop method to DLL

--- DLL-Constructor.py
+++ DLL-Constructor.py
@@ -21,18 +21,34 @@ class DoublyLinkedList:
             self.tail.next = new_node # Pointing the tail of the last node added to the list to the "new_node"
             new_node.prev = self.tail # The pointer to the previous node "prev" will point where "tail" is pointing 
             self.tail = new_node
-        self.length +=1
+        self.length += 1
         return True 
 
-    def print_list(self):
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev # the previous relative to what tail was pointing 
+            self.tail.next = None # Break the connection ->
+            temp.prev = None # Break the connection <-
+        self.length -= 1
+        return temp
+
+    def print_list(self): # Add the temp.value to check the value inside the nodes
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
-  
 
 my_doubly_linked_list = DoublyLinkedList(7)
 my_doubly_linked_list.append(2)
+my_doubly_linked_list.pop()
+my_doubly_linked_list.pop()
+my_doubly_linked_list.pop()
 
 my_doubly_linked_list.print_list()
 
