95d19016ed09f2d536891d07f364b185564411e6
SebastianLopez55 <lopezsebastian856@gmail.com>
2022-05-23 20:35:49 -0700

    add insert method

--- DLL-Constructor.py
+++ DLL-Constructor.py
@@ -84,17 +84,35 @@ class DoublyLinkedList:
             return True
         return False
 
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
+
+        new_node.prev = before 
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node 
+        
+        self.length += 1
+        return True 
+        
     def print_list(self): # Add the temp.value to check the value inside the nodes
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
 
-my_doubly_linked_list = DoublyLinkedList(0)
-my_doubly_linked_list.append(1)
-my_doubly_linked_list.append(2)
+my_doubly_linked_list = DoublyLinkedList(1)
 my_doubly_linked_list.append(3)
-my_doubly_linked_list.set_value(0,4)
+my_doubly_linked_list.insert(1,2)
 
 my_doubly_linked_list.print_list()
 
