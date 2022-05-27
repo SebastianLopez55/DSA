8380bdfa0d8a6a72e22adc66c010de5bb49c2100
SebastianLopez55 <lopezsebastian856@gmail.com>
2022-05-27 11:59:48 -0700

    Add push method to stack data structure

--- Stack.py
+++ Stack.py
@@ -15,7 +15,19 @@ class Stack:
             print(temp.value)
             temp = temp.next 
 
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top # New node is going to point where top is pointing to 
+            self.top = new_node
+        self.height += 1
+
+print('\n')
+
 my_stack = Stack(4)
+my_stack.push(1)
 my_stack.print_stack()
 
         
