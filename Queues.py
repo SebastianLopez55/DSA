25856a339d8ae903dc4bf99b9e9153b897a45e88
SebastianLopez55 <lopezsebastian856@gmail.com>
2022-06-06 22:36:02 -0700

    feat: add de_que method

--- Queues.py
+++ Queues.py
@@ -28,10 +28,23 @@ class Queue:
            self.last.next = new_node
            self.last = new_node
         self.length += 1
-    
+
+    def de_queue(self):
+        if self.length == 0:
+            return None 
+        temp = self.first 
+        if self.length == 1:
+            self.first = None
+            self.last = None 
+        else:
+            self.first = self.first.next 
+            temp.next = None
+        self.length -= 1
+        return temp    
 
 my_queue = Queue(0)
 my_queue.en_queue(1)
 my_queue.en_queue(2)
+my_queue.de_queue()
 
 my_queue.print_queue()
